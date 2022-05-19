d3484364d3c0d5f2844bef421cf03b239a67f89e
Doctuh <kkarlsson0@gmail.com>
2022-05-19 09:27:39 +0200

    fucked something up

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path('',Home.as_view()),
     path('profile/', ProfileList.as_view(), name='profiles'),
     path('profile/create', CreateProfile.as_view(), name='create_profile'),
-    path('home/<str:profile_id>/', ProfileHome.as_view(), name='profileHome'),
-    path('movie/detail/<str:movie_id>/', MovieDetail.as_view(), name='show_movie'),
-    path('movie/play/<str:movie_id>', ShowMovie.as_view(), name='movie_play'),
+   
 ]
